3ac0b3872d225225a7a261ac37e068175ca30075
elarsaks <elarsaks@gmail.com>
2019-07-06 19:22:01 +0300

    Angular app ready!

---
+++ angular-test/src/app/menu/menu.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+
+export class MenuComponent {
+
+  @Output() addEvent = new EventEmitter<number>();
+  @Output() createEvent = new EventEmitter<number>();
+  @Output() removeEvent = new EventEmitter<number>();
+
+  addRows(val :number){
+    this.addEvent.emit(val)
+  }
+
+  createRows(val :number){
+    this.createEvent.emit(val)
+  }
+
+  removeRows(){
+    this.removeEvent.emit(0)
+  }
+}
